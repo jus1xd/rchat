845338d8d3a41802ab7edf52661facbaed430248
Daniil <lxqtpr@gmail.com>
2021-03-15 19:32:16 +0400

    footer

---
+++ src/app/chat-input/chat-input.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './chat.input.module.css'
+import Smile from '../../img/ChatInput/smile.svg'
+import Camera from '../../img/ChatInput/camera.svg'
+import Clip from '../../img/ChatInput/clip.svg'
+import Microphone from '../../img/ChatInput/microphone.svg'
+
+const ChatInput: React.FC = (): JSX.Element => {
+   return (
+      <div className={s.chat__input}>
+         <a href="/#"><img src={Clip} alt="clip" className={s.clip} /></a>
+         <input  placeholder='Введите текст сообщения...' className={s.input} type="text" />
+         <div className={s.chat__input__btn}>
+            <a href="/#"><img src={Camera} alt="camera" className={s.camera} /></a>
+            <a href="/#"><img src={Smile} alt="smile" className={s.microphone} /></a>
+            <a href="/#"> <img src={Microphone} alt="microphone" className={s.microphone} /></a>
+         </div>
+      </div>
+   )
+}
+
+export default ChatInput
--- src/app/content/messages/message/message.tsx
+++ src/app/content/messages/message/message.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import s from './message.module.css';
 
-type TOwnProps = {own?: boolean}
+type TOwnProps = { own?: boolean }
 
 
 
-const Message: React.FC<TOwnProps> = ({own}): JSX.Element => {
+const Message: React.FC<TOwnProps> = ({ own }): JSX.Element => {
 
     return (
         <div className={own ? s.message__wrapper : `${s.message__wrapper} ${s.own}`}>
-            <div className={s.message__author}/>
+            <div className={s.message__author} />
             <div className={s.message__inner}>
                 <div className={s.message__content}>
-                Кек. Ты то меня никогда не предашь
+                    Кек. Ты то меня никогда не предашь
                 </div>
                 <div className={s.message__date}>
                     Вчера, в 12:30
